268c4c4ab2fbe33f099fc6ebb134289d65a03a96
SeanDuffie <sean.duffie@tamu.edu>
2023-03-25 18:10:19 -0500

    Cleanup lora for micropython

--- node/LoRa.py
+++ node/LoRa.py
@@ -5,33 +5,8 @@ Git Repo: https://github.com/Lightning-N-a-Bottle/lnb-node
 Main Doxygen: https://lightning-n-a-bottle.github.io/lnb-node/docs/html/index.html
 LoRa Doxygen: https://lightning-n-a-bottle.github.io/lnb-node/docs/html/namespacenode_1_1_lo_ra.html
 """
-from .gpio import lora_rx, lora_tx
-from .constants import MPY
-
-if not MPY:
-    import logging
-
-# def init() -> str:
-#     """ Initializes the Lora connection
-
-#     This will acquire the GPS location and attempt to send it to the server
-#     If successful, the server will respond with a string, which will become the reference
-#     name for the local node.
-#     Afterwards the GPS module should be shut down.
-
-#     Args:
-#         None
-#     Returns:
-#         name (str): the name received from the Server
-#     """
-#     packet = gps()
-
-#     send(packet)        # Send GPS data to Raspberry Pi
-    
-#     name = "Node"
-#     # while name is None:     # Loop until a name is given
-#     #     name = lora_rx()    # Receives new name from the Raspberry Pi FIXME: Add a timeout if it takes too long
-#     return name
+# from .gpio import lora_tx
+# from .constants import MPY
 
 def send(packet: str) -> None:
     """ Processes Main LoRa communications with packet transfer
@@ -51,22 +26,9 @@ def send(packet: str) -> None:
     # else:
 
     # Send Packet
-    lora_tx(packet)
+    # lora_tx(packet)
 
-    # Confirm delivery FIXME: should this be an affirmation or an accuracy check?
-    response = "None"
-    # while response is None:     # Loop until a response is found
-    #     response = lora_rx()    # Receives a confirmation of reception
-    
-    if response == "windows":
-        logging.info("\t%s\t|\tRunning in Windows Dev Mode: %s", __name__, packet)
-    elif response == "disabled":
-        logging.info("\t%s\t|\tLoRa Module Disabled: %s", __name__, packet)
-    else:
-        if MPY:
-            print(f"{__name__}\t|\tDELIVERED={packet}")
-        else:
-            logging.info("\t%s\t|\tDELIVERED=%s\n", __name__, packet)
+    print(f"{__name__}\t|\tDELIVERED={packet}")
 
     # else:
         # logging.error("\t%s\t|\tResponse was different...", __name__)
