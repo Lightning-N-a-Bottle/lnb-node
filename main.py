38d136cb6549812f10fe4330837a357828edf79e
SeanDuffie <sean.duffie@tamu.edu>
2023-04-07 21:06:04 -0500

    typo

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Documentation Notes:
     - https://stackoverflow.com/questions/1523427/what-is-the-common-header-format-of-python-files
     - https://www.tutorialspoint.com/python/python_multithreading.htm
 TODO:
-    - implement threading in circuitpython (if available)v
+    - implement threading in circuitpython (if available)
     - sleep and wake from ls spi connection to save power (probably not needed)
 """
 import sys
