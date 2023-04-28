aaaea2719197acc2e44acb4b9c5ac9cb975b4440
SeanDuffie <sean.duffie@tamu.edu>
2023-04-27 20:21:40 -0500

    Bring in changes from pico

--- node/constants.py
+++ node/constants.py
@@ -6,6 +6,7 @@ Main Doxygen: https://lightning-n-a-bottle.github.io/lnb-node/docs/html/index.ht
 Constants Doxygen: https://lightning-n-a-bottle.github.io/lnb-node/docs/html/namespacenode_1_1constants.html
 """
 import os
+
 MPY: bool = False   # Running MicroPython
 CORES: int = 1      # System Core Count (For multithreading)
 RPI: bool = False   # Running on a Raspberry Pi
@@ -16,12 +17,15 @@ try:
     RPI = True
 except AttributeError:
     import platform
+
     ### IDENTIFY SYSTEM CORE COUNT ###
     # CORES = os.cpu_count()
-
     ### IDENTIFY SYSTEM OS/GPIO CAPABILITY ###
     if platform.system() == "Linux" and platform.release().find('raspi'):
         RPI = True
+
+### MODULE NAME (FOR FILE SORTING) ###
+NAME: str = "NODE-A"
 
 ### WHAT MODULES ARE CURRENTLY CONNECTED ###
 LS: bool = True         # Lightning Sensor
@@ -34,3 +38,4 @@ RTC: bool = False       # Real Time Clock [True for external, False for internal
 NOISE_FLOOR: int = 5        # (1-7, default=2) Lower to detect smaller strikes, with more noise
 WATCHDOG_THRESH: int = 2    # (1-10, default=2) TODO:
 SPIKE_REJECT: int = 1       # (1-11, default=2) Modify the shape of spikes, round with lower range
+TUNE_CAP: int = 0           # (0-120, default=0) Modify the tune capacitor 
--- node/sensor.py
+++ node/sensor.py
@@ -5,7 +5,7 @@ It will be responsible for the GPIO interface with sensor equipment
 
 Git Repo: https://github.com/Lightning-N-a-Bottle/lnb-node
 Main Doxygen: https://lightning-n-a-bottle.github.io/lnb-node/docs/html/index.html
-LoRa Doxygen: https://lightning-n-a-bottle.github.io/lnb-node/docs/html/namespacenode_1_1sensor.html
+Sensor Doxygen: https://lightning-n-a-bottle.github.io/lnb-node/docs/html/namespacenode_1_1sensor.html
 """
 import sys
 import time
@@ -15,9 +15,11 @@ import board
 import busio
 import digitalio
 import rtc
+from adafruit_datetime import datetime
 
 # Module Constants
-from .constants import GPS, LS, NOISE_FLOOR, SPIKE_REJECT, WATCHDOG_THRESH
+from .constants import (GPS, LS, NOISE_FLOOR, SPIKE_REJECT, TUNE_CAP,
+                        WATCHDOG_THRESH, NAME)
 
 
 class Sensor:
@@ -44,9 +46,11 @@ class Sensor:
         self.gps_lat, self.gps_long = self.get_GPS_Fix()
 
         # Generate the filename to append the values to (a new file is generated after each reboot)
-        self.filename = f"{self.timestamp()}_({self.gps_lat},{self.gps_long})"
+        self.filename = f"{NAME}_{time.time()}_({self.gps_lat},{self.gps_long})"
 
         # Turn off GPS Module after Fix or if disabled in constants.py
+        # Sleep required to ensure the GPS value is saved.
+        time.sleep(.5)
         self.GPS_ENABLE.value = 0
 
         # Setup Lightning Sensor Module
@@ -81,10 +85,11 @@ class Sensor:
             else:
                 print(f"{__name__}\t| DEBUG - The Lightning Detector is in an Unknown mode.")
 
-            # Callibrate - If these parameters should be changed, then do so in py
+            # Callibrate - If these parameters should be changed, then do so in constants.py
             self.as3935.noise_level = NOISE_FLOOR
             self.as3935.watchdog_threshold = WATCHDOG_THRESH
             self.as3935.spike_rejection = SPIKE_REJECT
+            self.as3935.tune_cap = TUNE_CAP
         else:
             # Set up Interrupt pin on GPIO D21 with a pull-down resistor
             self.as3935_interrupt_pin = digitalio.DigitalInOut(board.GP8)
@@ -129,12 +134,19 @@ class Sensor:
             # Set update rate to once a second (1hz) which is what you typically want.
             gps_module.send_command(b"PMTK220,1000")
 
+            # Variable to test whether GPS has a timestamp
+            check_y = 2020
             # Wait until the GPS obtains a fix
-            while not gps_module.has_fix:
+            while not gps_module.has_fix or check_y == 2020:
                 print("Waiting for fix...")
-                gps_module.update()     # Refreshes "has_fix" value
+                gps_module.update()                         # Refreshes "has_fix" value
+                check_y = gps_module.timestamp_utc.tm_year   # Checks that time has been acquired
                 time.sleep(1)
             print(f"{__name__}\t| DEBUG - Got GPS Fix!")
+
+            # Store GPS location
+            gps_lat = gps_module.latitude
+            gps_long = gps_module.longitude
 
             # Set RTC using Fix timestamp
             self.clock.datetime = time.struct_time(
@@ -151,15 +163,6 @@ class Sensor:
                 )
             )
 
-            # Store GPS location
-            # gps.latitude_degrees, gps.latitude_minutes
-            # gps.longitude_degrees, gps.longitude_minutes
-            # gps.fix_quality
-            # gps.satellites
-            # gps.altitude_m
-            # gps.speed_knots
-            gps_lat = gps_module.latitude
-            gps_long = gps_module.longitude
 
         else:
             gps_lat: float = -1
@@ -167,7 +170,7 @@ class Sensor:
 
         return gps_lat, gps_long
 
-    def timestamp(self) -> float:
+    def timestamp(self) -> str:
         """ Acquire current time from Real Time Clock Module.
 
         This uses the internal Pico RTC module, but alternatively it can be used with an external
@@ -175,30 +178,49 @@ class Sensor:
         The value stored in this RTC is initialized from the GPS Fix, however if the GPS is
         disabled or disconnected, it will use a default value instead.
 
-        NOTE: If we choose to use a different timestamp value there are 3 other options already available:
+        NOTE: If we choose to use a different timestamp value there are 2 options already available:
             - time_int = an integer that represents the amount of time that has passed since Jan 1st, 1970
-            - timestruc* = a struct/tuple that contains 9 elements with information about the current time
-                *(see https://docs.circuitpython.org/en/latest/shared-bindings/time/index.html#time.struct_time for more info)
-            - timestring = a formatted string that shows a more human-readable version of the time, but not csv/code friendly
+            - dt* = an object that contains all info about a date object and a time object, can be printed
+                *(see https://docs.circuitpython.org/projects/datetime/en/latest/index.html for more info)
+            - time_stamp = a utc formatted string that shows a more human-readable version of the time
+
+        UTC - YYYY-MM-DDThh:mm:ssZ
+            - sec = epoch % 60
+            - min = (epoch // 60) % 60
+            - hrs = (epoch // 3600) % 24
+            - day = epoch // 86400
+        Epoch
+            - 1 hour = 3600 seconds
+            - 1 day = 86400 seconds
+            - 1 week = 604800 seconds
+            - 1 month = 2629743 seconds
+            - 1 year = 31556929 seconds
 
         Args:
             None
         Returns:
             time_int (int): amount of seconds that have passed since Jan 1, 1970
+                or
+            time_stamp (str): utc time formatted in ISO8601 format
         """
-        time_int: float = time.time()
-        timestruc: time.struct_time = time.localtime(time_int)
-        timestring: str = f"{timestruc.tm_year}-{timestruc.tm_mon}-{timestruc.tm_yday}_{timestruc.tm_hour}:{timestruc.tm_min}:{timestruc.tm_sec}"
-        print(f"{__name__}\t| Current time: {timestring}")
-        return time_int
+        # Acquires the utc formatted time
+        dt: datetime = datetime.now()
+        time_stamp: str = dt.isoformat(sep="T")
 
-    def lightning(self) -> str:
+        # time_int: int = time.time()       # DEPRICATED, but still kept for reference
+
+        return time_stamp
+
+    def lightning(self) -> float:
         """ Interacts with the Lightning Sensor Module
+
+        This function will be blocking until the AS3935 measures either a strike or a disturber
 
         Args:
             None
         Returns:
-            ls_out (str): a concatenated string with sensor data for the packet
+            distance (float): the distance measured by the lightning sensor, in kilometers
+                - [63 if disturber, -1 if disabled]
 
         GPIO Pins Involved:
         - CS ["Chip Select"]: Pull low to activate SPI reception
@@ -208,8 +230,7 @@ class Sensor:
         - MOSI: Data from microcontroller to AS3935
 
         """
-        distance = "disabled"
-        intensity = "disabled"
+        distance = -1
 
         try:
             i = 0
@@ -227,29 +248,32 @@ class Sensor:
                         if interrupt_value == self.as3935.NOISE:
                             print(f"{__name__}\t| DEBUG - Noise.")
                             self.as3935.clear_statistics()
+
                         elif interrupt_value == self.as3935.DISTURBER:
                             i += 1
+
                             print(f"{__name__}\t| INFO - Disturber {i} detected {distance}km away!")
                             self.as3935.clear_statistics()
                             # Comment out break to not save to csv
                             break
+
                         elif interrupt_value == self.as3935.LIGHTNING:
-                            print(f"{__name__}\t| INFO - Lightning strike detected {distance}km away!")
                             #Turn on PiLED
                             self.PiLED.value = 1
+
+                            print(f"{__name__}\t| INFO - Lightning strike detected {distance}km away!")
                             self.as3935.clear_statistics()
                             break
 
                     else:
                         break
+
                     time.sleep(0.5)
                     self.PiLED.value = 0
         except KeyboardInterrupt:
             pass
 
-        ls_out = f"{distance},{intensity}"
-
-        return ls_out
+        return distance
 
 
     def collect(self) -> str:
@@ -264,12 +288,14 @@ class Sensor:
             packet (str): The properly formatted packet to be passed to LoRa
         """
         # When lightning is detected, this will populate the string with the sensor data
-        stk: str = self.lightning()     # Acquire Lightning Distance/Intensity
+        stk: float = self.lightning()     # Acquire Lightning Distance/Intensity
         # Acquire RTC Timestamp, this has to come after the lightning strike
-        tstmp: float = self.timestamp()   # Acquire the formatted timestruct
+        t_stmp: str = self.timestamp()   # Acquire the UTC formatted timestruct
+        # Epoch time, used for graphing data
+        t_int: float = time.time()
 
         # Append to PACKET_QUEUE
-        packet: str = f"{tstmp},{self.gps_lat},{self.gps_long},{stk}"
+        packet: str = f"{t_stmp},{t_int},{self.gps_lat},{self.gps_long},{stk}"
         print(f"{__name__}\t| CREATED={packet}")
 
         return packet
